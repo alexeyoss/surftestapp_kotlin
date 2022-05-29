d12302ae8189bfce7185ff025e4edd29d44a8556
alexey_oss <mukovozov19281@gmail.com>
2022-05-29 23:37:32 +0300

    bugs fixed

--- app/src/main/java/com/oss/surftesttask_kotlinversion/data/repository/Repository.kt
+++ app/src/main/java/com/oss/surftesttask_kotlinversion/data/repository/Repository.kt
@@ -8,27 +8,14 @@ import com.oss.surftesttask_kotlinversion.retrofit.entities.NetworkMapper
 import com.oss.surftesttask_kotlinversion.utils.DataState
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
-class Repository
-@Inject
-constructor(
+
+class Repository(
     private val resultDao: ResultDao,
     private val retrofit: ApiService,
     private val resultMapper: ResultCacheMapper,
     private val networkMapper: NetworkMapper
 ) {
-//    @Inject
-//    private lateinit var resultDao:ResultDao
-//    @Inject
-//    private lateinit var retrofit:ApiService
-//    @Inject
-//    private lateinit var resultMapper:ResultCacheMapper
-//    @Inject
-//    private lateinit var networkMapper:NetworkMapper
-
     suspend fun getMovies(): Flow<DataState<List<Results>>> = flow {
         emit(DataState.Loading)
         try {
